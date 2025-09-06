e5a1eeaf4bca88d29797300ee82fd86edec3b72a
zaqy-ramadhan <ramadhanz619@gmail.com>
2025-09-06 18:47:03 +0700

    test2

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
-      base: "./",
+      base: process.env.VITE_BASE_PATH || "/qoar-photo",
       plugins: [react()],
       resolve: {
         alias: {
